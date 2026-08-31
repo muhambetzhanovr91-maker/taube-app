f2edcef5837f00bc83fa7d644d1dc37879369b9f
muhambetzhanovr91-maker <muhambetzhanovr91@gmail.com>
2026-08-31 11:50:44 +0500

    Create QiblaScreen.kt

---
+++ app/QiblaScreen.kt
@@ -0,0 +1,16 @@
+package kz.taube.app
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun QiblaScreen() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text("2. Құбыла бағыты (Компас)", fontSize = 18.sp, fontWeight = FontWeight.Bold)
+    }
+}
